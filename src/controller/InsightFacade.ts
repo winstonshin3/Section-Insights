348037953efffc888530fbc236fe803bb51c5f41
RyanKm1 <ryankim.k19@gmail.com>
2024-03-15 00:40:05 -0700

    writeJson might be the culprit?

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -56,13 +56,13 @@ export default class InsightFacade implements IInsightFacade {
 		let fileNames = Object.keys(fileInFolder);
 		// TODO didn't validate folder before
 		// console.log("What is this: " + fileInFolder["courses/"].name);
+		await fs.ensureDir("./data");
 		if (kind === "sections") {
 			validateSectionsFiles(fileNames);
 			let filteredFileNames = filterSectionFileNames(fileNames);
 			let contentsInZip = await getContentsOfFiles(filteredFileNames, zip, id);
 			let cacheData: object = makeInsightResult(id, kind, contentsInZip);
-			// await fs.ensureDir("./data");
-			// await fs.writeJson(`./data/${id}`, cacheData);
+			await fs.writeJson(`./data/${id}`, cacheData);
 		}
 		if (kind === "rooms") {
 			validateRoomsFiles(fileNames);
